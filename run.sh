3f8a8d79e85e09dde11344e2fcd178d36409d8c0
epl <me@example.com>
2022-07-27 05:27:38 +0200

    auto

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 wget https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-gpl.tar.xz 
 tar xf ffmpeg-master-latest-linux64-gpl.tar.xz
 rm -v ffmpeg-master-latest-linux64-gpl.tar.xz
-cd ffmpeg* && mv -vf  bin/ffmpeg ~/harj&&chmod a+x ~/harj
+cd ffmpeg* && mv -vf  bin/ffmpeg ../harj&&chmod a+x ../harj
 cd .. && rm -rf ffmpeg*
